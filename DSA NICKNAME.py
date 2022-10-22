9bc17ee72cebb055b29e01bf5aa32d818d90fe9d
Jomari Miguel M  Carlos <116429305+JomariMiguel-Carlos@users.noreply.github.com>
2022-10-22 18:06:11 +0800

    Add files via upload

---
+++ DSA NICKNAME.py
@@ -0,0 +1,13 @@
+print("************** PROGRAMMED BY *************")
+print("******** Jomari Miguel M. Carlos *********")
+print("**************** BSCOE 2-2 ***************")
+print("********* Sir Danilo Madrigalejos ********")
+
+print("")
+print("  *******    *         *")
+print("     *       **       **")
+print("     *       * *     * *")
+print(" *   *       *  *   *  *")
+print(" *   *       *   * *   *")
+print(" *****       *    *    *")
+
